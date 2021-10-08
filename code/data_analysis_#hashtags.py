2c6f0de24649df607565d08d4cebf364ea6ab414
sweedp <hendrik-timm@hotmail.de>
2021-10-08 15:09:21 +0200

    rename data_analysis to data_analysis_#hashtags and move code to hashtags.py

--- code/data_analysis_#hashtags.py
+++ code/data_analysis_#hashtags.py
@@ -14,41 +14,24 @@ import numpy as np
 with open("data/feature_extraction/training.pickle", "rb") as f_in:
     data = pickle.load(f_in)
 
-
-num_hashtags = np.zeros((len(data["labels"]),1))
-
-for i in range(len(data["features"][0])):
-    num_hashtags[i][0] = data["features"][0][i].count(",")
-    
-    if(len(data["features"][0][i])>2):
-        num_hashtags[i][0] +=1
-   
-
-features = num_hashtags
+features = data["features"]
 labels = data["labels"]
-
 
 pos = features[labels]
 neg_index = np.array([not x for x in labels])
 neg = features[neg_index]
 
 
-bins = [-1,0, 1,2,3,4,5,6,7,8,9,10]
+bins = [0, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 
-
-
-
-    
 plt.hist(features, bins = bins, color='blue',label=" all labels")
-
 plt.hist(neg, bins = bins, color='red', label="negative labels")
-
 plt.hist(pos, bins = bins, color='green', label= "positive labels", )
-
 plt.legend()
-
 plt.xlabel("Number of Hashtags")
 plt.ylabel("Number of Tweets")
+
 # count the specific numbers of positive/negative label for different #hashtags, here for example for 0 hashtags.
-print(np.count_nonzero(pos == 0))
-print(np.count_nonzero(neg == 0))
+no_hashtags = 5
+print("Number of viral tweets with ", no_hashtags, " hashtags", np.count_nonzero(pos == no_hashtags))
+print("Number of lame tweets with ", no_hashtags, " hashtags", np.count_nonzero(neg == no_hashtags))
