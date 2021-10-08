e11f20510f35cd2c1d70fde5fb08aeeb79755c58
sweedp <hendrik-timm@hotmail.de>
2021-10-08 14:40:34 +0200

    working data_analysis
    working data_analysis.py file. Though I still need to shift the feature
    extraction to the hashtag.py file.

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -13,36 +13,42 @@ import numpy as np
 
 with open("data/feature_extraction/training.pickle", "rb") as f_in:
     data = pickle.load(f_in)
-#print(list(data["features"]))
-print(data["features"][0][0])
-print((data["features"][0][0]).count(",")+1)
-
-print(data["features"][0][1])
-print(len(data["features"][0][1]))
 
 
-#TODO:
-    # insert number of hashtags of each entry in a new array of the same size as the labels/overall tweets.
-    # graphically see if there is any difference
+num_hashtags = np.zeros((len(data["labels"]),1))
 
-num_hashtags = np.zeros(len(data["labels"]))
-print(len(data["labels"]))
-print(len(num_hashtags))
+for i in range(len(data["features"][0])):
+    num_hashtags[i][0] = data["features"][0][i].count(",")
+    
+    if(len(data["features"][0][i])>2):
+        num_hashtags[i][0] +=1
+   
 
-for hashtags in data["features"][0]:
-    pass
-
-
-#print(data["labels"][100])
-
-features = data["features"]
+features = num_hashtags
 labels = data["labels"]
-print(features)
 
-# plt.hist(features)
-#plt.hist(features, range = [0,400])
 
 pos = features[labels]
 neg_index = np.array([not x for x in labels])
 neg = features[neg_index]
 
+
+bins = [-1,0, 1,2,3,4,5,6,7,8,9,10]
+
+
+
+
+    
+plt.hist(features, bins = bins, color='blue',label=" all labels")
+
+plt.hist(neg, bins = bins, color='red', label="negative labels")
+
+plt.hist(pos, bins = bins, color='green', label= "positive labels", )
+
+plt.legend()
+
+plt.xlabel("Number of Hashtags")
+plt.ylabel("Number of Tweets")
+# count the specific numbers of positive/negative label for different #hashtags, here for example for 0 hashtags.
+print(np.count_nonzero(pos == 0))
+print(np.count_nonzero(neg == 0))
