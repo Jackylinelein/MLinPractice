8ed08c4a0d9533b47d663ef1d3f6e5509c9a5995
sweedp <hendrik-timm@hotmail.de>
2021-10-08 15:52:42 +0200

    two different plot strategies, added percentage plot
    First plot strategy is to plot the amount of all tweets and the number
    of hashtags
    
    Second plot strategy is to have a graph which indicates the percentage
    of viral tweets in correspondance to the amount of hashtags.
    For example with 5 hashtags the probability to have a viral tweet is 15%
    instead of 10% (overall dataset)

--- code/data_analysis_#hashtags.py
+++ code/data_analysis_#hashtags.py
@@ -11,6 +11,10 @@ import pickle
 from matplotlib import pyplot as plt
 import numpy as np
 
+show = 'TOTAL'
+show = 'PERCENTAGE'
+
+
 with open("data/feature_extraction/training.pickle", "rb") as f_in:
     data = pickle.load(f_in)
 
@@ -24,14 +28,29 @@ neg = features[neg_index]
 
 bins = [0, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 
-plt.hist(features, bins = bins, color='blue',label=" all labels")
-plt.hist(neg, bins = bins, color='red', label="negative labels")
-plt.hist(pos, bins = bins, color='green', label= "positive labels", )
-plt.legend()
-plt.xlabel("Number of Hashtags")
-plt.ylabel("Number of Tweets")
+if(show=='TOTAL'):
+    # total number of tweets
+    plt.hist(features, bins = bins, color='blue',label=" all labels")
+    plt.hist(neg, bins = bins, color='red', label="negative labels")
+    plt.hist(pos, bins = bins, color='green', label= "positive labels", )
+    plt.legend()
+    plt.xlabel("Number of Hashtags")
+    plt.ylabel("Number of Tweets")
 
 # count the specific numbers of positive/negative label for different #hashtags, here for example for 0 hashtags.
-no_hashtags = 5
-print("Number of viral tweets with ", no_hashtags, " hashtags", np.count_nonzero(pos == no_hashtags))
-print("Number of lame tweets with ", no_hashtags, " hashtags", np.count_nonzero(neg == no_hashtags))
+
+elif(show=='PERCENTAGE'):
+    viral_tweets = np.zeros((30,1))
+    line = np.ones((30,1))
+    for i in range(30):
+        line[i][0] = 0.1
+        viral = np.count_nonzero(pos == i)
+        lame = np.count_nonzero(neg == i)
+        if(viral+lame>0):
+            viral_tweets[i][0] = viral/(lame+viral)
+        
+    plt.plot(viral_tweets, label='percent of viral tweets')
+    plt.plot(line, color = 'red', label = "threshold")
+    plt.legend()
+    
+     
