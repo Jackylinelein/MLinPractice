7d3657e87127a94c2d59cc73d23c49310cc384c2
sweedp <hendrik-timm@hotmail.de>
2021-10-08 13:04:54 +0200

    create data_analysis.py to evaluate the number of hashtags

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -6,8 +6,6 @@ Created on Thu Oct  7 13:01:20 2021
 @author: Hendrik Timm
 """
 
-
-
 # plotting with matplotlib
 import pickle
 from matplotlib import pyplot as plt
@@ -15,12 +13,34 @@ import numpy as np
 
 with open("data/feature_extraction/training.pickle", "rb") as f_in:
     data = pickle.load(f_in)
+#print(list(data["features"]))
+print(data["features"][0][0])
+print((data["features"][0][0]).count(",")+1)
 
-features = data["hashtags"]
+print(data["features"][0][1])
+print(len(data["features"][0][1]))
+
+
+#TODO:
+    # insert number of hashtags of each entry in a new array of the same size as the labels/overall tweets.
+    # graphically see if there is any difference
+
+num_hashtags = np.zeros(len(data["labels"]))
+print(len(data["labels"]))
+print(len(num_hashtags))
+
+for hashtags in data["features"][0]:
+    pass
+
+
+#print(data["labels"][100])
+
+features = data["features"]
 labels = data["labels"]
+print(features)
 
 # plt.hist(features)
-plt.hist(features, range = [0,400])
+#plt.hist(features, range = [0,400])
 
 pos = features[labels]
 neg_index = np.array([not x for x in labels])
