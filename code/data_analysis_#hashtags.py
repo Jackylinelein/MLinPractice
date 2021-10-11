b28b92c2608318f7dd56209b4db823fc6abbed7e
sweedp <hendrik-timm@hotmail.de>
2021-10-11 10:41:59 +0200

    change comments in data_analysis_#hashtags

--- code/data_analysis_#hashtags.py
+++ code/data_analysis_#hashtags.py
@@ -15,6 +15,8 @@ show = 'TOTAL'
 show = 'PERCENTAGE'
 
 
+## TODO: Write own data_analysis graph with methods for percentage of viral tweets w.r.t. something and total number.
+
 with open("data/feature_extraction/training.pickle", "rb") as f_in:
     data = pickle.load(f_in)
 
@@ -28,6 +30,7 @@ neg = features[neg_index]
 
 bins = [0, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 
+# plot Number of tweets w.r.t. number of hashtags
 if(show=='TOTAL'):
     # total number of tweets
     plt.hist(features, bins = bins, color='blue',label=" all labels")
@@ -37,8 +40,8 @@ if(show=='TOTAL'):
     plt.xlabel("Number of Hashtags")
     plt.ylabel("Number of Tweets")
 
-# count the specific numbers of positive/negative label for different #hashtags, here for example for 0 hashtags.
 
+# plot percentage of viral tweets w.r.t. njmber of hashtags
 elif(show=='PERCENTAGE'):
     viral_tweets = np.zeros((30,1))
     line = np.ones((30,1))
