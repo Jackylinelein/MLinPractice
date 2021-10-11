fac4d7ddbd6697cb55cfe71f3e13c4adcab573e0
Jackylinelein <jacky.xanten@gmail.com>
2021-10-11 10:40:30 +0200

    Implement sort lists.
    To ensure that list elements - of an input list - which include the same
    elements are treated as equal a sort function was implemented. Moreover
    a special test case was implemented for this.

--- test/preprocessing/counter_test.py
+++ test/preprocessing/counter_test.py
@@ -16,24 +16,37 @@ class CounterTest(unittest.TestCase):
         self.INPUT_COLUMN ="input"
         self.OUTPUT_COLUMN = "output"
         self.counter = Counter(self.INPUT_COLUMN, self.OUTPUT_COLUMN)
-        
+            
         
     def test_input_columns(self):
         self.assertListEqual(self.counter._input_columns, [self.INPUT_COLUMN])
 
+
     def test_output_columns(self):
         self.assertEqual(self.counter._output_column, self.OUTPUT_COLUMN)
 
-    def test_counting(self):
-        input_name = ["element1","element2","element1","element1","element3"]
-        output_count = 3
-        
+
+    def test_counting_elements(self):
+        # input needs to be a pandas data frame like in run_preprocessing.py
         input_df = pd.DataFrame()
-        input_df[self.INPUT_COLUMN] = input_name
+        input_df[self.INPUT_COLUMN] = ["element1","element2","element1","element1","element3"]
+        output_count = 3
         
         counted = self.counter.fit_transform(input_df)
         self.assertEqual(counted[self.OUTPUT_COLUMN][0], output_count)
 
+
+    def test_counting_list_elements(self):
+        # input needs to be a pandas data frame like in run_preprocessing.py
+        input_df = pd.DataFrame()
+        list_1 = ["element1","element2"]
+        list_1_1 = ["element2","element1"]
+        list_2 = ["element1","element1"]
+        input_df[self.INPUT_COLUMN] = [list_1, list_1_1, list_1, list_2, list_2, list_1]
+        output_count = 4
+        
+        counted = self.counter.fit_transform(input_df)
+        self.assertEqual(counted[self.OUTPUT_COLUMN][0], output_count)
 
 # needed to run script using the spyder console
 if __name__ == '__main__':
