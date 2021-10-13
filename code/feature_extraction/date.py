ae07b2b9f00ed83709f7e1963cc6092bc41f8157
sweedp <hendrik-timm@hotmail.de>
2021-10-13 16:26:46 +0200

    clean up code

--- code/feature_extraction/date.py
+++ code/feature_extraction/date.py
@@ -8,6 +8,8 @@ Created on Tue Oct 12 12:15:12 2021
 
 import numpy as np
 from code.feature_extraction.feature_extractor import FeatureExtractor
+import pandas as pd
+
 
 # class for extracting the number of hashtags in a tweet as a feature
 class Year(FeatureExtractor):
@@ -18,20 +20,14 @@ class Year(FeatureExtractor):
     
     
     def _get_values(self, inputs):
-       
-        #TODO one-hot-encoded
-        #TODO SLICE ARRAY OF FORMAT
-        #2020-05-10; YEAR-MONTH-DAY
+
         year_array = np.zeros((len(inputs[0]),1)) 
         counter = 0
         for year in inputs[0]:
            year_array[counter] = year[0:4]
            counter+=1
-        print(year_array)
-        
-        
+
         return year_array
-    
     
 class Month(FeatureExtractor):
    
@@ -42,43 +38,87 @@ class Month(FeatureExtractor):
    
    
    def _get_values(self, inputs):
-      
-       #TODO one-hot-encoded
-       #TODO SLICE ARRAY OF FORMAT
-       #2020-05-10; YEAR-MONTH-DAY
-       
+
        month_array = np.zeros((len(inputs[0]),1)) 
        counter = 0
        for month in inputs[0]:
           month_array[counter] = month[5:7]
           counter+=1
-       print(month_array)
-       
-       
-       return month_array
+         
+          return month_array
     
-# TODO need to look up the weekday in some package of date probably.
-class WeekDay(FeatureExtractor):
+class Day(FeatureExtractor):
    
     
    # constructor
    def __init__(self, input_column):
-       super().__init__([input_column], "{0}_month".format(input_column))
+       super().__init__([input_column], "{0}_day".format(input_column))
    
    
    def _get_values(self, inputs):
-      
-       #TODO one-hot-encoded
-       #TODO SLICE ARRAY OF FORMAT
-       #2020-05-10; YEAR-MONTH-DAY
-       
-       month_array = np.zeros((len(inputs[0]),1)) 
+
+       day_array = np.zeros((len(inputs[0]),1)) 
        counter = 0
-       for month in inputs[0]:
-          month_array[counter] = month[5:7]
+       for day in inputs[0]:
+          day_array[counter] = day[8:10]
           counter+=1
-       print(month_array)
        
-       
-       return month_array
+       return day_array
     
+class Weekday(FeatureExtractor):
+    
+    
+    #constructor
+    def __init__(self, input_column):
+        super().__init__([input_column], "{0}_weekday".format(input_column))
+                         
+        
+    def _get_values(self, inputs):
+        
+        
+        weekday_array = np.zeros((len(inputs[0]),1))
+        counter = 0
+        for weekday in inputs[0]:
+            temp = pd.Timestamp(weekday)
+            weekday_array[counter] = temp.dayofweek
+            counter+=1
+        
+        return weekday_array
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                         
+                        
