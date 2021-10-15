61a209e580b1c2e2de222dbbd695cb3f14562aa8
Jackylinelein <jacky.xanten@gmail.com>
2021-10-15 14:43:59 +0200

    Change max_iter in classification step of pipeline

--- code/classification.sh
+++ code/classification.sh
@@ -19,7 +19,7 @@ python -m code.classification.run_classifier data/dimensionality_reduction/train
 # Classifier Decion Tree
 python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle --decision_tree 5 -s 42 --accuracy --kappa --recall --f_measure 
 # Classifier Support Vector Machine
-python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle --support_vector_machine 2000 -s 42 --accuracy --kappa --recall --f_measure 
+python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle --support_vector_machine 10000 -s 42 --accuracy --kappa --recall --f_measure 
 
 # run feature extraction on validation set (with pre-fit extractors)
 echo "  validation set"
