ef59f7374036f9db3a3d794d746c039492d12ee6
sweedp <hendrik-timm@hotmail.de>
2021-10-31 16:40:43 +0100

    run data_analysis in shell script

--- code/feature_extraction.sh
+++ code/feature_extraction.sh
@@ -6,8 +6,9 @@ mkdir -p data/feature_extraction/
 # run feature extraction on training set (may need to fit extractors)
 echo "  training set"
 
-python -m code.feature_extraction.extract_features data/preprocessing/split/training.csv data/feature_extraction/training.pickle -e data/feature_extraction/pipeline.pickle --char_length --hashtags --date --tweet_total_count --wanted_str data
-
+python -m code.feature_extraction.extract_features data/preprocessing/split/training.csv data/feature_extraction/training.pickle -e data/feature_extraction/pipeline.pickle --char_length --hashtags --date --tweet_total_count --wanted_str data 
+# run data_analysis for plotting graphs
+python -c'import code.data_analysis; code.data_analysis.plot()'
 
 # run feature extraction on validation set and test set (with pre-fit extractors)
 echo "  validation set"
